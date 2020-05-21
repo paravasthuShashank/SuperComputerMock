d1b77510ff4b6ec8b599ec3dab2097d210aa2974
paravasthuShashank <65531951+paravasthuShashank@users.noreply.github.com>
2020-05-21 20:06:43 +0530

    Update application.py

--- application.py
+++ application.py
@@ -14,5 +14,40 @@ def attributes():
     return jsonify(data)
 app.add_url_rule('/ufmRest/monitoring/attributes','attributes',attributes,'GET')
 
+def resources():
+    f = open("./result.json")
+    data = json.load(f)
+    f.close()
+    return jsonify(data)
+app.add_url_rule('/ufmRest/resources/systems', 'resources', resources,'GET')
+
+def start():
+    return """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+
+<title>Redirecting...</title>
+
+<h1>Redirecting...</h1>
+
+<p>You should be redirected automatically to target URL: <a href="/ufmRest/monitoring/session/10618">/ufmRest/monitoring/session/10618</a>.  If not click the link. """
+app.add_url_rule('/ufmRest/monitoring/start', 'start', start, 'POST')
+
+def data():
+    sessionFile = open('./session.txt','w')
+    sessionFile.write(str(1000))
+    sessionFile.close()
+    # sessionId = 10628
+    f = open("./resultData.json")
+    data = json.load(f)
+    f.close()
+    return jsonify(data)
+app.add_url_rule('/ufmRest/monitoring/session/1000/data', 'data', data, 'GET')
+
+def delete():
+    return """delete page"""
+sessionFile = open('./session.txt','r')
+sessionId = sessionFile.read().strip()
+sessionFile.close()
+deleteUrl = '/ufmRest/monitoring/session/' + sessionId
+app.add_url_rule(deleteUrl,'delete', delete)
 
 # app.run()
