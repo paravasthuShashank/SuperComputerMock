e32e9050d61e19c6f6804aa409e14bf8647be3a9
paravasthuShashank <65531951+paravasthuShashank@users.noreply.github.com>
2020-05-21 20:01:09 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,8 +1,10 @@
 from flask import Flask
+import json
+from flask import request, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello govind!"
+    return "Hello narasimha!"
 
 # app.run()
